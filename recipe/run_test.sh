486dd09529ac75111315abdf842e50a5e670f660
Isuru Fernando <isuruf@gmail.com>
2019-08-05 09:41:48 -0500

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -11,8 +11,10 @@ if [[ "$CONDA_PY" != "27" ]]; then
     grep "built-in method kill" gdb_output
 fi
 # Unfortunately python 3.6 package does not have enough debug info for py-bt
-if [[ "$CONDA_PY" != "36" ]]; then
+if [[ "$CONDA_PY" != "36" && "$CONDA_PY" != "27" ]]; then
     grep "line 3" gdb_output
     grep "process_to_debug.py" gdb_output
     grep 'os.kill(os.getpid(), signal.SIGSEGV)' gdb_output
 fi
+
+grep "Program received signal SIGSEGV" gdb_output
