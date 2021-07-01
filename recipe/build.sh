11bfce04b3cddace04ccb00d844a9a8037fbda84
Philippe Blain <levraiphilippeblain@gmail.com>
2021-07-01 01:07:14 -0400

    recipe: add '-fno-blocks' on macOS to work around header collision
    Version 11 of libc++ that is pulled in by the conda-forge build
    infrastructure includes llvm/llvm-project@430b00954c ([libc++] Add
    support for blocks in std::function, 2020-04-22) [1] that makes the
    'functional' standard library header depend on Block.h, at least on
    macOS, which seems to have _LIBCPP_HAS_EXTENSION_BLOCKS defined by
    default. This header can be found at
    $OSX_SDK_DIR/MacOSX10.9.sdk/usr/include/Block.h, and is used for
    Apples's "Blocks" language extension [2].
    
    This leads to a badly failing build because the header files "block.h" in GDB
    (in gdb/) and "Block.h" collide since the macOS filesystem is case
    insensitive, and the former is included instead of the latter [3]:
    
    ~~~
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/work/gdbsupport/agent.cc:20:
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/work/gdbsupport/common-defs.h:123:
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/work/gdbsupport/ptid.h:35:
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/_build_env/bin/../include/c++/v1/functional:512:
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/work/gdbsupport/../gdb/Block.h:23:
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/work/gdbsupport/../gdb/dictionary.h:26:
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/work/gdbsupport/../gdb/symfile.h:24:
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/work/gdbsupport/../gdb/symtab.h:23:
    In file included from
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/_build_env/bin/../include/c++/v1/array:116:
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/_build_env/bin/../include/c++/v1/algorithm:1503:81:
    error: expected '(' for function-style cast or type construction
        return _VSTD::__search<typename
    add_lvalue_reference<_BinaryPredicate>::type>
                               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
    /Users/travis/miniforge3/conda-bld/gdb_1624656007191/_build_env/bin/../include/c++/v1/algorithm:1503:19:
    error: no member named '__search' in namespace 'std::__1'
        return _VSTD::__search<typename
    add_lvalue_reference<_BinaryPredicate>::type>
               ~~~~~~~^
    ~~~
    
    Disable the "blocks" extension on macOS to work around this filename
    collision [4].
    
    [1] https://reviews.llvm.org/rG430b00954c674241f16549290ab49cc69f93a431
    [2] https://en.wikipedia.org/wiki/Blocks_(C_language_extension)
    [3] https://travis-ci.com/github/conda-forge/gdb-feedstock/jobs/519258376#L4420-L4425
    [4] https://releases.llvm.org/11.0.0/tools/clang/docs/ClangCommandLineReference.html#cmdoption-clang-fblocks

--- recipe/build.sh
+++ recipe/build.sh
@@ -50,6 +50,7 @@ if [[ $target_platform == "osx-64" ]]; then
   # Copy the activate script to the installation prefix
   mkdir -p "${PREFIX}/etc/conda/activate.d"
   cp $RECIPE_DIR/activate.sh "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
+  export CXXFLAGS="${CXXFLAGS} -fno-blocks"
 fi
 
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
