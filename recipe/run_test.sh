4e9fcdc7e6b91a4cd09a9c9991d4020758f75530
Daniel Ching <dching@nvidia.com>
2026-06-02 14:16:16 -0500

    BLD: Remove mold from test script

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -19,14 +19,7 @@ if [[ $(uname -m) == "ppc64le" || $(uname -m) == "aarch64" ]]; then
 fi
 
 # Run hello world test
-if [[ $(uname) == "Darwin" && $(uname -m) == "x86_64" ]]; then
-  # `-fuse-ld=mold` not supported on macOS x86-64
-  $CC -o hello -g "$RECIPE_DIR/testing/hello.c"
-else
-  # TODO: Remove mold usage once https://github.com/conda-forge/binutils-feedstock/issues/97
-  # is done.
-  $CC -o hello -g -gz=zstd -fuse-ld=mold "$RECIPE_DIR/testing/hello.c"
-fi
+$CC -o hello -g "$RECIPE_DIR/testing/hello.c"
 gdb -batch -ex "run" --args hello
 
 # This next test tries to simulate a crash on a python process. The process under test
